de963a9602d66165cffdf4b7696cc7b33edd5ddd
Onuh Chukwuma <onuhchukwuma@gmail.com>
2022-01-02 05:32:40 +0100

    Configured dbContext

---
+++ CompanyEmployee.Entities/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using CompanyEmployee.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyEmployee.Entities
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options) { }
+        
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
