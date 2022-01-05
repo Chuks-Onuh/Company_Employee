322ce6c91e0d558db0f9a12080898c3a22e119d4
Onuh Chukwuma <onuhchukwuma@gmail.com>
2022-01-05 19:39:55 +0100

    Implemented global error handling

--- CompanyEmployee.API/Controllers/CompaniesController.cs
+++ CompanyEmployee.API/Controllers/CompaniesController.cs
@@ -28,22 +28,11 @@ namespace CompanyEmployee.API.Controllers
         [HttpGet]
         public IActionResult GetCompanies()
         {
-            try
-            {
-                var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+            var companies = _repository.Company.GetAllCompanies(trackChanges: false);
 
-                var resp = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+            var resp = _mapper.Map<IEnumerable<CompanyDto>>(companies);
 
-                return Ok(resp);
-            }
-            catch (Exception ex)
-            {
-
-                _logger.LogError($"Something went wrong in the {nameof(GetCompanies)} action {ex}");
-
-                return StatusCode(500, "Internal server error");
-
-            }
+            return Ok(resp);
         }
     }
 }
--- CompanyEmployee.API/Startup.cs
+++ CompanyEmployee.API/Startup.cs
@@ -1,4 +1,5 @@
 using CompanyEmployee.API.Extensions;
+using CompanyEmployee.Contracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
@@ -35,13 +36,14 @@ namespace CompanyEmployee.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerManager logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.ConfigureExceptionHandler(logger);
             app.UseHttpsRedirection();
 
             app.UseStaticFiles(); app.UseCors("CorsPolicy"); 
