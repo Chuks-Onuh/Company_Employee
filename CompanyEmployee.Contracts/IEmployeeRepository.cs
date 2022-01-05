5280601646950b3626bf22959dbfa51a65ac8dc2
Onuh Chukwuma <onuhchukwuma@gmail.com>
2022-01-05 10:54:18 +0100

    Created generic repo for Crud operation

---
+++ CompanyEmployee.Contracts/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyEmployee.Contracts
+{
+    interface IEmployeeRepository
+    {
+    }
+}
---
+++ CompanyEmployee.Repository/RepositoryBase.cs
@@ -0,0 +1,35 @@
+﻿using CompanyEmployee.Contracts;
+using CompanyEmployee.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace CompanyEmployee.Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private readonly ApplicationContext _context;
+
+        public RepositoryBase(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public System.Linq.IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ? _context.Set<T>()
+                            .AsNoTracking() : _context.Set<T>();
+
+
+        public System.Linq.IQueryable<T> FindByCondition(System.Linq.Expressions.Expression<Func<T, bool>> expression, bool trackChanges) =>
+            !trackChanges ? _context.Set<T>()
+                            .Where(expression)
+                            .AsNoTracking() : _context.Set<T>()
+                            .Where(expression);
+
+        public void Create(T entity) => _context.Set<T>().Add(entity);
+
+        public void Update(T entity) => _context.Set<T>().Update(entity);
+
+        public void Delete(T entity) => _context.Set<T>().Remove(entity);
+    }
+}
